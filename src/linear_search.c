4f52988c46ae707d5914020248c86ce314d8fffb
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-04-30 10:31:47 +0000

    feat: create linear_search.c

---
+++ src/linear_search.c
@@ -0,0 +1,42 @@
+/**
+ * @file linear_search.c
+ * @author MRDGH2821 (ask.mrdgh2821@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-04-30
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void int_scan(int *number) {
+  char s[10] = {};
+  fgets(s, sizeof(number), stdin);
+  *number = strtol(s, NULL, 10);
+}
+
+int main() {
+  int a[10];
+  int num, i, flag;
+
+  printf("Enter 5 numbers:\n");
+  for (i = 0; i < 5; i++) {
+    int_scan(&a[i]);
+  }
+  printf("Enter number to be searched");
+  int_scan(&num);
+  for (i = 0; i < 5; i++) {
+    if (num == a[i]) {
+      flag = 1;
+      break;
+    }
+  }
+  if (flag == 1) {
+    printf("Number found at %d", i + 1);
+  } else {
+    printf("Number not found");
+  }
+}
