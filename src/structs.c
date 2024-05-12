9cc038d7641a16efcbfa3cf121b1b2e8cbd7d794
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-05-12 07:52:28 +0000

    refactor: use secure ways to take input

--- src/structs.c
+++ src/structs.c
@@ -1,7 +1,21 @@
-// name, id, dept, salary. o/p - whose details you want. - 4 names as options.
-// display details.
+/**
+ * @file structs.c
+ * @author MRDGH2821 (ask.mrdgh2821@outlook.com)
+ * @brief Display details of employees using structures.
+ * @version 0.1
+ * @date 2024-05-12
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 
 #include <stdio.h>
+#include <stdlib.h>
+void int_scan(int *number) {
+  char s[10] = {};
+  fgets(s, sizeof(number), stdin);
+  *number = strtol(s, NULL, 10);
+}
 struct employee {
   int id, salary;
   char name[40], dept[40];
@@ -13,28 +27,21 @@ int main() {
   for (int i = 0; i < 3; i++) {
     printf("Enter details of Employee %d", i + 1);
     printf("Name:");
-    scanf("%s", e[i].name);
+    fgets(e[i].name, sizeof(e[i].name), stdin);
     printf("Department:");
-    scanf("%s", e[i].dept);
+    fgets(e[i].dept, sizeof(e[i].dept), stdin);
     printf("ID:");
-    scanf("%d", &e[i].id);
+    int_scan(&e[i].id);
     printf("Salary:");
-    scanf("%d", &e[i].salary);
-  };
+    int_scan(&e[i].salary);
+  }
 
-  int c;
-
+  int choice;
   printf("Whose details to find (1,2,3,4)?");
-
-  scanf("%d", &c);
-
-  c = c - 1;
-
-  printf("Name: %s\n", e[c].name);
-
-  printf("Department: %s\n", e[c].dept);
-
-  printf("ID: %d\n", e[c].id);
-
-  printf("Salary: %d\n", e[c].salary);
+  int_scan(&choice);
+  choice = choice - 1;
+  printf("Name: %s\n", e[choice].name);
+  printf("Department: %s\n", e[choice].dept);
+  printf("ID: %d\n", e[choice].id);
+  printf("Salary: %d\n", e[choice].salary);
 }
