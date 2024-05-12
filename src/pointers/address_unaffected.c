d236e152d7ff2de7001bed403cc5dc0008c66b9c
MRDGH2821 <ask.mrdgh2821@outlook.com>
2024-05-12 10:54:23 +0000

    style: split brief in 2 lines

--- src/pointers/address_unaffected.c
+++ src/pointers/address_unaffected.c
@@ -1,7 +1,8 @@
 /**
  * @file address_unaffected.c
  * @author MRDGH2821 (ask.mrdgh2821@outlook.com)
- * @brief Demonstrates that the address of a variable remains unaffected even if the variable is modified
+ * @brief Demonstrates that the address of a variable remains unaffected even if
+ * the variable is modified
  * @version 0.1
  * @date 2024-04-30
  *
